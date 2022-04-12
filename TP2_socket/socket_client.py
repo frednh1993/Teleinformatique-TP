8fce4a6bee9b3cb9a7fed8cce2998abca9ac5ae7
Frederik Boutin <Frederik.Boutin@uqar.ca>
2022-04-12 00:31:37 -0400

    Projet final

--- TP2_socket/socket_client.py
+++ TP2_socket/socket_client.py
@@ -1,7 +1,7 @@
-# Importation des librairies pour les sockets et les fichiers
+# Importation des librairies pour les sockets, les options de fichier et pour la fonction split (via re)
 import socket
 import os
-# longueurEntete = 10
+import re
 
 # Attribution du mode UDP, de l'adresse IP et du numéro de port du socket Client :
 sock_Client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,7 +9,7 @@ sock_Client.bind(("127.0.0.1", 5653))
 
 
 
-# Le client se connect au port du serveur et envoit un premier message de synchronisation :
+# Le Client se connect au port du Serveur et envoit un premier message de synchronisation (msg_syn).
 try:
     sock_Client.connect(("127.0.0.1", 22222))
     msg_syn = "Hello Serveur"
@@ -21,14 +21,14 @@ except:
 
 
 
-# Client va écouter le message d'acquittement du Serveur (dans msg_ack) : 
+# Le Client va écouter le message d'acquittement du Serveur (msg_ack) : 
 while True:
-    msg_ack, address = sock_Client.recvfrom(4096)
+    msg_ack, address = sock_Client.recvfrom(1100)
 
     if msg_ack:
         msg_ack = msg_ack.decode(encoding='utf-8') 
 
-        # Si le Client reçoit le bon message d'acquittement, il va envoyer le message final qui confirme la connexion (Three-way handshake) : 
+        # Si le Client reçoit le bon message d'acquittement, il va envoyer le message final (via msg_con) qui confirme la connexion (Three-way handshake) : 
         if msg_ack == "Hello Client":
             print(f"Client : Message d'acquittement {msg_ack} reçu du Serveur {address}. \n")
 
@@ -39,53 +39,73 @@ while True:
 
 
 
-fichier = open("Copie_fichier.png", "wb")
-checksum = ""
+# 1100 oct. de réception pour avoir une sécurité avec l'entête qui est attachée aux données.
+info_fichier_Serveur, address = sock_Client.recvfrom(1100)
+info_fichier_Serveur = info_fichier_Serveur.decode(encoding='utf-8')
+#
+info_fichier_Serveur = info_fichier_Serveur.split(' ')
+nom_fichier_Serveur = info_fichier_Serveur[0]
+taille_fichier_Serveur = info_fichier_Serveur[1]
 
-nom_fichier_copie, address = sock_Client.recvfrom(1100)
-nom_fichier_copie = nom_fichier_copie.decode(encoding='utf-8') 
+#print(nom_fichier_Serveur)
+#print(taille_fichier_Serveur)
+
+# Ouverture du fichier de copie en écriture binaire.
+fichier = open(f"Copie_{nom_fichier_Serveur}", "wb")
+# checksum : variable qui donne l'ordre des datagrammes pour la vérification de similarité du fichier transmis.
+checksum = ""
  
 while True:
 
-    # Réception dans la variable data de chaque d'atagramme transmis par le serveur (data = Header + datagramme)
+    # Réception, dans la variable data, de chaque datagramme transmis par le Serveur (data = Header + datagramme de données)
     data, address = sock_Client.recvfrom(1100)
 
-    # Suite à la réception du dernier datagramme de données, le serveur envoit le segment -END- au client qui va mettre fin à la transcription du fichier de copie
+    # Suite à la réception du dernier datagramme de données, le Serveur envoit le segment -END- au client pour mettre fin à la transcription du fichier de copie.
     if data == b'-END-':
-        print("Encore la fin")
-        # Fermeture du socket client et du fichier de copie
+        # Fermeture du socket Client et du fichier en cours.
         sock_Client.shutdown(socket.SHUT_RDWR) 
         sock_Client.close() 
         fichier.close()    
         break
 
-    # Variable substring qui va contenir l'entête du datagramme 
-    start = data.find(b'{S.HEADER}') 
-    end = data.find(b'{E.HEADER}') + len(b'{E.HEADER}')
-    substring = data[start:end]
+    # Si data est null, il n'y aura pas de Header.
+    if data is not None:
+        # Variable substring qui va contenir l'entête du datagramme. 
+        start = data.find(b'{S.HEADER}') 
+        end = data.find(b'{E.HEADER}') + len(b'{E.HEADER}')
+        substring = data[start:end]
 
-    # Retrait de l'entête de la variable data
-    data = data.replace(substring, b'')
+        # Retrait de l'entête des données de data.
+        data = data.replace(substring, b'')
 
-    # Stockage de lentête complète dans la variable (str) substring 
-    substring = substring.decode()
+        # Stockage de lentête dans la variable (str) substring. 
+        substring = substring.decode()
         
-    # Pour la vérification de transmission de chaque datagramme, on stocke seulement le numéro de chaque datagramme reçu dans la variable checksum
-    début = substring.find(f"{{ND}}") + len(f"{{ND}}")
-    fin = substring.find(f"{{TD}}") 
-    substring = substring[début:fin]
+        # Pour la vérification de transmission de chaque datagramme, on stocke seulement le numéro de chaque datagramme reçu dans la variable checksum.
+        début = substring.find(f"{{ND}}") + len(f"{{ND}}")
+        fin = substring.find(f"{{TD}}") 
+        substring = substring[début:fin]
 
-    # Client envoit le numéro du datagramme lu au serveur comme confirmation :
-    sock_Client.send(substring.encode())
+        # Client envoit le numéro du datagramme lu au Serveur comme confirmation.
+        sock_Client.send(substring.encode())
 
-    checksum += substring + ";"
+        checksum += substring + ";"
 
-    # Écriture de chaque segement de données des datagrammes reçus dans la variable fichier
-    fichier.write(data)
+        # Écriture de chaque segement de données reçus (datagramme de données) dans la variable fichier.
+        fichier.write(data)
+    
 
+# Taille du fichier copier.
+#print(os.path.getsize(f"Copie_{nom_fichier_Serveur}"))
+# Pour voir l'ordre des datagrammes reçus.
+#print(checksum)
 
-print(checksum)
-print(f"Client : Le fichier {nom_fichier_copie} a été copié côté Client.")
+# Si la taille du fichier copier = taille du fichier Serveur = bonne transmission de données.
+# Petite ERREUR : nous avons toujour une taille de fichier copier légèrement inférieur au fichier Serveur !?
+if taille_fichier_Serveur == os.path.getsize(f"Copie_{nom_fichier_Serveur}"):
+    print(f"Client : Le fichier {nom_fichier_Serveur} a été copié côté Client sans Erreur.")
+
+print(f"Client : Le fichier {nom_fichier_Serveur} a été copié côté Client.")
 
 
 
--- TP2_socket/socket_serveur.py
+++ TP2_socket/socket_serveur.py
@@ -1,19 +1,20 @@
-# Importation des librairies pour les sockets, les fichiers et le random (aléatoire)
+# INSTRUCTION : Entrez le nom du fichier à transmettre à la ligne 45 : nom_fichier = "". Votre fichier doit se trouver à l'intérieur du document TP2_socket
+
+# Importation des librairies pour les sockets, les options de fichier, le random (aléatoire) ainsi que les fonctions mathématiques.
 import socket
 import os
 import random
 import math
 
-# Attribution du mode UDP, de l'adresse IP et du numéro de port du socket Serveur :
+# Attribution du mode UDP, de l'adresse IP et du numéro de port du socket Serveur.
 sock_Serveur = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 sock_Serveur.bind(("127.0.0.1", 22222))
-#sock_Serveur.listen(5)
 
 
 
-# Réception du message de synchronisation du Client, connexion du Serveur au Client, envoit de message d'acquittement du Serveur et confirmation de connexion :
+# Réception du message de synchronisation du Client, connexion du Serveur au Client, envoit du message d'acquittement au Client et confirmation de connexion.
 while True:
-   msg_syn, address = sock_Serveur.recvfrom(4096)
+   msg_syn, address = sock_Serveur.recvfrom(1100)
 
    if msg_syn:
       msg_syn = msg_syn.decode(encoding='utf-8')
@@ -33,78 +34,85 @@ while True:
 
 
 
-# nbrDgm : Nombre de datagramme envoyé, 
+# nbrDgm : Nombre de datagrammes envoyés, 
 # oct_init : Représente le début du curseur pour la transmission des datagrammes (ex : Datagramme1 oct_init=0; Datagramme2 oct_init=1001; ...),
-# fin : Code de fin de transmission une fois tout le fichier transmis au client.
+# fin : Code de fin de transmission du fichier envoyé au Client une fois tout le fichier transmis.
 nbrDgm = 1
 oct_init = 0
 fin = b'-END-'
 
-# Sélectionner le fichier à transmettre et obtenir sa taille :
+# Sélectionne le fichier à transmettre et obtient sa taille.
 nom_fichier = "Mario.png"
 fichierSize = os.path.getsize(nom_fichier)
 #print(fichierSize)
+# info_fichier renseigne le Client sur le nom du fichier transmis avec sa taille.
+info_fichier = nom_fichier + f" {fichierSize}"
 
-# Nombre total de datagrammes à transmettre
+# totDgm : Nombre total de datagrammes à transmettre.
 totDgm = int(math.ceil((fichierSize/1000)))
    
-# Ouverture en lecture binaire de la totalité du fichier à transmettre :
+# Ouverture en lecture binaire de la totalité du fichier à transmettre.
 fichier = open(nom_fichier, 'rb')
 fichierTot = fichier.read()
 
-# Envoit au Client du nom du fichier que l'on transmet : 
-sock_Serveur.send(str.encode(nom_fichier, encoding="utf-8"))
+# Envoit au Client du nom du fichier que l'on transmet.
+sock_Serveur.send(str.encode(info_fichier, encoding="utf-8"))
 
-# Boucle qui envoit un datagramme tant que tout le fichier n'est pas transmis en totalité (par tranche de 1000 oct. max de données) : (+1 ligne 60)
+
+
+# Boucle qui envoit un datagramme de données tant que tout le fichier n'est pas transmis (par tranche de 1000 oct. de données max).
 while nbrDgm <= totDgm:  
 
-   datagramme = fichierTot[oct_init:(nbrDgm*1000)]
+   if nbrDgm != totDgm:
+      datagramme = fichierTot[oct_init:(nbrDgm*1000)]
+   else:
+      datagramme = fichierTot[oct_init:fichierSize]
 
    header = f'{{S.HEADER}}{{ND}}{nbrDgm}{{TD}}{totDgm}{{E.HEADER}}'
    header = header.encode()
 
-   # datagramme envoyé = header + datagramme
+   # datagramme envoyé = header(entête indiquant le datagramme transmis par son numéro d'ordre ET le nombre total de datagrammes à transmettre) + datagramme 1000 oct. max de données.
    datagramme = b''.join([header, datagramme])
-   #print(type(datagramme))
 
-   # Afin de 
+   # Afin de simuler des erreurs d'envoi de datagramme, on utilise la fonction random 0-100 et 5 valeurs qui font envoyer un message vide (donc aucun message).
    fiabilite_reseau = random.randint(0,100)
 
    if fiabilite_reseau != 20 and fiabilite_reseau != 40 and fiabilite_reseau != 60 and fiabilite_reseau != 80 and fiabilite_reseau != 100:
-      # Transmission du datagramme :
+      # Transmission du datagramme.
       sock_Serveur.send(datagramme)
    else:
-      sock_Serveur.send(b"")
+      sock_Serveur.send(b'')
            
-   # Le client envoit une confirmation (confirmation_dgm) de chaque datagramme reçu :
-   confirmation_dgm, address = sock_Serveur.recvfrom(4096)
+   # Le client envoit une confirmation (confirmation_dgm) de chaque datagramme reçu.
+   confirmation_dgm, address = sock_Serveur.recvfrom(1100)
 
-   # Condition : Si le nombre de datagramme envoyé = totalité du fichier à transmettre = message de fin (+1 ligne 84)
+   # Condition : Si le nombre de datagramme envoyé = totalité des datagrammes du fichier à transmettre, on envoit le message de fin de transmission.
    if nbrDgm == totDgm:
       sock_Serveur.send(fin)
-      print("Fin de transmission")
+      print("Serveur : Fin de transmission")
 
-   # Si le Serveur reçoit confirmation_dgm, il va le caster de bytes en int (confirmation_dgm = numéro datagramme reçu par le Client) :
+   # Si le Serveur reçoit confirmation_dgm, il va le caster en int.
    if confirmation_dgm:
       confirmation_dgm = int(confirmation_dgm)
 
-   # Si on a la confirmation Client de la réception du datagramme, il va y avoir incrémentation de l'oct_init et du nombre de datagramme 
-   # (prochain datagramme à transmettre) :
+   # Si on a la confirmation Client de la réception du datagramme, il va y avoir incrémentation de oct_init et nbrDgm afin de transmettre le prochain datagramme dans la file.
    if confirmation_dgm == nbrDgm:
       oct_init = (nbrDgm*1000)+1
       nbrDgm = (nbrDgm + 1)
    else:
-      # S'il n'y a pas de réception du client, on démarre le timer de 3 sec et on regarde encore si nous avons reçu une confirmation :
+      # S'il n'y a pas de confirmation du Client, le timer de 3 sec est amorcé et la confirmation client est vérifiée de nouveau.
       sock_Serveur.settimeout(3)
       if confirmation_dgm == nbrDgm:
          oct_init = (nbrDgm*1000)+1
          nbrDgm = (nbrDgm + 1)
-# Si aucune confirmation Client, même suite au timer de 3 sec, la boucle va recommancer sans incrémenter vers le prochain datagramme (va donc
-# renvoyer le même datagramme) 
+# Si aucune confirmation Client n'est reçue (même suite au timer de 3 sec), la boucle va recommancer sans incrémenter vers le prochain datagramme (va donc renvoyer le même datagramme) 
 
 
 
-# Fermeture du fichier et du socket serveur :
+
+
+
+# Fermeture du fichier et du socket Serveur :
 fichier.close()
 sock_Serveur.shutdown(socket.SHUT_RDWR) 
 sock_Serveur.close()
